19937823bdb6f42d9bb1de0c61f302a44afb8d1f
unknown <you@example.com>
2020-06-16 11:58:36 -0300

    blogapp

--- app.js
+++ app.js
@@ -113,7 +113,7 @@
 
     app.use('/admin', admin)
     app.use('/usuarios', usuarios)
-const PORT = process.env.PORT ||8081
+const PORT = process.env.PORT || 3000
 app.listen(PORT, function(){
     console.log("Servidor rodando...")
 })
